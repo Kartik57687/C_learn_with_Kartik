ecfbc6c6c5cea46db906bf974088107747e8484f
Kartik Taralekar <76401134+Kartik57687@users.noreply.github.com>
2025-10-15 19:21:13 +0530

    Update 1.c

--- 1.c
+++ 1.c
@@ -2,12 +2,13 @@
 int main(){
   //Pattern In c 
   int n,m,j,i;
-  printf("Enter the No of rows and columns in your pattern: );
+  printf("Enter the No of rows and columns in your pattern: ");
+  scanf("%d%d",&n,&m);
     for(i=0;i<n;i++){
-    for(i=0;i<m;i++){
+    for(j=0;j<m;j++){
     printf("*");
     }
 printf("\n");
     }
-  return 0:
+  return 0;
 }
