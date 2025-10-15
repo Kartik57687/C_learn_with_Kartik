90d264865924f19ceef58fae521f09e8552f2e6c
Kartik Taralekar <76401134+Kartik57687@users.noreply.github.com>
2025-10-15 19:07:54 +0530

    Create 1.c
    Basic Pattern in C

---
+++ 1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+  //Pattern In c 
+  int n,m,j,i;
+  printf("Enter the No of rows and columns in your pattern: );
+    for(i=0;i<n;i++){
+    for(i=0;i<m;i++){
+    printf("*");
+    }
+printf("\n");
+    }
+  return 0:
+}
